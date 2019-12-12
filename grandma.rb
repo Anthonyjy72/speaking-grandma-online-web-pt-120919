72a8421eb9900e25ddd2f5c36951b023697e5cba
Anthony Ybarra <anthony_jy72@yahoo.com>
2019-12-12 19:06:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,10 @@
 
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif string != string.upcase
     "HUH?! SPEAK UP, SONNY!"
+  elsif string == string.upcase
+  return "NO, NOT SINCE 1938!"
   end
 end
